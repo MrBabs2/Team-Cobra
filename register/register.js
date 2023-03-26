daab26f32f6eff7b664e282478568b030c34db4e
otiteakpofure <akpofure.otite@gmail.com>
2023-03-26 18:57:52 +0100

    added a new feature - Geolocation API

--- register/register.js
+++ register/register.js
@@ -1,1 +1,13 @@
  "use strict"
+
+
+//  setting the GEOlocation API
+
+if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function(){
+        console.log(position)
+    },function(){
+        alert("Your position could not be derived")
+    })
+
+
