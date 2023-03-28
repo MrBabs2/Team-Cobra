f144738d86ce593019ec19a5ccc5b6ed636eda7c
otiteakpofure <akpofure.otite@gmail.com>
2023-03-28 13:00:49 +0100

    added prevent default actions to my form

--- register/register.js
+++ register/register.js
@@ -1,13 +1,25 @@
  "use strict"
-
+//  DOM element Manipulations 
+ const firstname = document.getElementById("firstname");
+ const form=document.getElementsByTagName("form");
 
 //  setting the GEOlocation API
 
 if(navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(){
-        console.log(position)
+        console.log("I am working")
     },function(){
         alert("Your position could not be derived")
     })
+
+
+    // setting event listeners 
+
+
+    //adding prevent default to the form to prevent its default behavior
+form.addEventListener("submit",(e)=>{e.preventDefault()})
+
+
+
 
 
