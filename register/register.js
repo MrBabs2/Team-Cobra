1bb95c8daaf34829d88c4f00e51cd6e2e9359ee6
otiteakpofure <akpofure.otite@gmail.com>
2023-03-29 20:50:53 +0100

    added validation checkers using regex for email addresses

--- register/register.js
+++ register/register.js
@@ -1,11 +1,18 @@
  "use strict"
 //  DOM element Manipulations 
  const form=document.getElementById("form_controller");
- const firstnamecheck = document.querySelector(".first-span")
- const lastNameCheck = document.querySelector(".lastname-span")
+ const firstnamecheck = document.querySelector(".first-span");
+ const lastNameCheck = document.querySelector(".lastname-span");
+ const emailCheck = document.querySelector(".email-span");
 
- // regex expression for firstname
- const firstnamepattern = /^[a-zA-Z]{6,10}$/  
+ // regex expression for firstname and lastname
+ const firstnamepattern = /^[a-zA-Z]{6,10}$/ 
+ // regex expression for email 
+ const emailchecker = /^.{4,}$/
+
+ //regex expression for the email address 
+
+
 //  setting the GEOlocation API
 if(navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(){
@@ -27,6 +34,8 @@ form.addEventListener("submit",(e)=>{e.preventDefault()
     }
      //regex expression using ternary operator for the lastname value 
     firstnamepattern.test(form.inputLastName.value)? lastNameCheck.style.display = "none"  : lastNameCheck.style.display = "inline"
+    //regex expression for testing emails
+    emailchecker.test(form.inputemail.value)?emailCheck.style.display="none":emailCheck.style.display="inline"
 })
 
 
