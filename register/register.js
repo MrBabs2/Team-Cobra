6cd83a902d533f5c71f2d9ff0876e85f6150b144
otiteakpofure <akpofure.otite@gmail.com>
2023-03-29 19:55:38 +0100

    added regex expression for the firstname input

--- register/register.js
+++ register/register.js
@@ -1,23 +1,33 @@
  "use strict"
 //  DOM element Manipulations 
- const firstname = document.getElementById("firstname");
- const form=document.getElementsByTagName("form");
+ const form=document.getElementById("form_controller");
+ const firstnamecheck = document.querySelector(".first-span")
 
+ // regex expression for firstname
+ const firstnamepattern = /^[a-zA-Z]{6,10}$/  
 //  setting the GEOlocation API
-
 if(navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(){
-        console.log("I am working")
+        console.log("Your Position has been dervied ")
     },function(){
         alert("Your position could not be derived")
     })
+// setting event listeners 
+ //adding prevent default to the form to prevent its default behavior
+ //seting validation for the input fields in the form 
+ //adding regex expression to the code 
+form.addEventListener("submit",(e)=>{e.preventDefault()
+    // regex expression for firstname
+    if(firstnamepattern.test(form.firstname.value)){
+       firstnamecheck.style.display = "none" 
+    }
+    else{
+        firstnamecheck.style.display = "inline" 
+    }
+})
 
 
-    // setting event listeners 
 
-
-    //adding prevent default to the form to prevent its default behavior
-form.addEventListener("submit",(e)=>{e.preventDefault()})
 
 
 
