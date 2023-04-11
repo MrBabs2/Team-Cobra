4f180f6ccfe33c257101c16520cf8cac5cd11e59
AdeolaIbiyeye <127867125+AdeolaIbiyeye@users.noreply.github.com>
2023-04-11 15:56:21 +0100

    libraypage JS for payment

---
+++ librarypage.js
@@ -0,0 +1,15 @@
+// Dom mainpultation for payment 
+const first = document.querySelector("#first-button")
+const second = document.querySelector("#second-button")
+const third = document.querySelector("#third-button")
+const fourth = document.querySelector("#fourth-button")
+const fifth = document.querySelector("#fifth-button")
+const sixth = document.querySelector("#sixth-button")
+const seventh = document.querySelector("#seventh-button")
+const eight = document.querySelector("#eight-button")
+const nine = document.querySelector("#ninth-button")
+const ten = document.querySelector("#tenth-button")
+const payButton = document.querySelector(".checkout")
+
+const buttons = [first,second,third,fourth,fifth,sixth,seventh,eight,nine,ten]
+ 
